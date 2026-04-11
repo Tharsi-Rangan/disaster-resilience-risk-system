b064598ccd1c821506268dd556f4b406f185cde7
SaayinathK <k.saayinath@gmail.com>
2026-04-11 13:08:10 +0530

    fixed console log message in terminal

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 const authRoutes = require("./routes/auth.routes");
 const projectRoutes = require("./routes/project.routes");
