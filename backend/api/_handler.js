88f311f16f4165aa41b5015719fc538f6ea87f2f
SaayinathK <k.saayinath@gmail.com>
2026-04-12 14:17:09 +0530

    deploy change 7

--- backend/api/_handler.js
+++ backend/api/_handler.js
@@ -33,7 +33,9 @@ function applyCorsHeaders(req, res) {
   const origin = getCorsOrigin(req.headers.origin);
   if (origin) {
     res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Vary", "Origin");
   }
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 }
